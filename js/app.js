517c57919097b44f1534c5c6ae1bcb704fe7e276
usigna <sylwia.fait@gmail.com>
2023-07-20 20:33:01 +0200

    Add base files

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// for production only
+// window.onerror = function(message, url, lineNumber) {  
+//   return true;
+// };
+
+// web accessibility - don't touch this
+function handleFirstTab(e) {
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing');
+    window.removeEventListener('keydown', handleFirstTab);
+  }
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
+function showHamburgerMenu() {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.navigation');
+  const navigationLinks = document.querySelectorAll('.navigation__link');
+
+  const handleClick = function() {
+    hamburger.classList.toggle('hamburger--active');
+    hamburger.setAttribute('aria-expanded', hamburger.classList.contains('hamburger--active'));
+    nav.classList.toggle('navigation--active');
+  }
+
+  for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener('click', function() {
+      hamburger.classList.remove('hamburger--active');
+      nav.classList.remove('navigation--active');
+    })
+  }
+
+  hamburger.addEventListener('click', handleClick);
+};
+
+const init = function() {
+  // showHamburgerMenu();
+};
+
+document.addEventListener('DOMContentLoaded', init);
